69079b29844e0b6a8afcd3a44e1ce73ea85d7551
Adam Compton <adam.compton@gmail.com>
2016-06-08 16:09:47 -0400

    added better error handling, timeouts, printing

--- PYTHON/jenkins_boradcast_scan.py
+++ PYTHON/jenkins_boradcast_scan.py
@@ -1,6 +1,8 @@
 from socket import *
+import re
 
 s = socket(AF_INET, SOCK_DGRAM)
+s.settimeout(1)
 s.bind(('', 0))
 s.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
 
@@ -8,4 +10,9 @@ data = repr(1) + '\n'
 s.sendto(data, ('<broadcast>', 33848))
 
 while 1:
-    print s.recvfrom(1024)
+    try:
+        data, peer = s.recvfrom(1024)
+        ver = re.match( r'.*version>(.*)<\/version.*', data)
+        print "%s - has Jenkins Version %s" % (peer[0], ver.group(1))
+    except timeout:
+        break
